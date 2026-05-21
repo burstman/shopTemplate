1b918ab1e492511462e5d11dfa1a7f4f1dc3afb8
burstman <hamedflissi@gmail.com>
2026-05-21 10:19:30 +0100

    cleanup: remove DashboardURL from Affiliate model

--- app/models/affiliate.go
+++ app/models/affiliate.go
@@ -12,7 +12,6 @@ type Affiliate struct {
 	Active          bool     `gorm:"default:true"`
 	APIToken        string   `gorm:"uniqueIndex;size:64"`
 	ShopURL         string   `gorm:"size:255"`
-	DashboardURL    string   `gorm:"size:255"`
 	APIKey          string   `gorm:"size:128"`
 	Balance         Currency `gorm:"type:numeric(12,2);default:100.00"`
 	AuthorizedEmail string   `gorm:"size:255"`
